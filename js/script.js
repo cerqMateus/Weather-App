0bfa064473b96eb357ec2eccd9bbbf47b1bd2e72
Mateus_2020 Cerqueira <mateus.cerq7@outlook.com>
2025-01-29 06:30:56 -0300

    Added media queries, loading feedback and refatored script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@ document.getElementById('search-button').addEventListener('click', function() {
     const city = document.getElementById('city-input').value;
     const apiKey = '44acdb9c47a73533f571ecd5b4274bb7'; 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${apiKey}`;
-
+    const spinner = document.getElementById('loading-spinner');
     const natureImages = [
         'background-1',
         'background-2',
         'background-3',
     ];
 
+    spinner.style.display = 'block'; // Mostrar o spinner
+
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+            spinner.style.display = 'none'; // Esconder o spinner
             document.getElementById('location').textContent = `${data.name}, ${data.sys.country}`;
             document.getElementById('temperature').textContent = `${data.main.temp}°C`;
             document.getElementById('condition').textContent = data.weather[0].description;
@@ -21,6 +24,7 @@ document.getElementById('search-button').addEventListener('click', function() {
             document.body.className = randomClass;
         })
         .catch(error => {
+            spinner.style.display = 'none'; // Esconder o spinner
             console.error('Erro ao buscar dados do clima:', error);
             alert('Cidade não encontrada. Tente novamente.');
         });
